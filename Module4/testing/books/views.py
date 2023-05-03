eb80ea7ae1ad8b2a1bfffde9d9001a23466b02ab
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2023-05-03 17:36:45 +0200

    Add books app to Module4

--- Module4/testing/books/views.py
+++ Module4/testing/books/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 from books.models import Book, Author
 
 
 def books_list(request):
    books = Book.objects.all()
+
+   paginator = Paginator(books, 3)
+   page_number = request.GET.get('page')
+   books = paginator.get_page(page_number)
+
    return render(
        request=request,
        template_name="books/books_list.html",
