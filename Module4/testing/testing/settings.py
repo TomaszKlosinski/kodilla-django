59069120f4e82a8f22a1ce831d712636b3ede97f
Tomasz Klosinski <tomasz.klosinski@movedigital.ch>
2023-05-03 13:01:17 +0200

    Add login/logout to Modile4

--- Module4/testing/testing/settings.py
+++ Module4/testing/testing/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login/logout redirect URLs
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
